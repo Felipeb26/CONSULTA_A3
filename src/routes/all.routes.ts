5d414b339fa7f9eab4b9711e29c801e765170799
felipe.silva <felipe.silva@grupomultiplica.com.br>
2022-11-30 09:56:09 -0300

    deploy

---
+++ src/routes/all.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { ConsultaController } from './../controller/consulta.controller';
+
+export const route = Router({ caseSensitive: false });
+
+const consult = new ConsultaController();
+
+//consultas endpoints
+route.get("/consultas",  consult.getAll);
+
+route.get("/consulta/:id",  consult.getById);
+
+route.get("/consultas/:param",  consult.getConsultas)
+
+route.post("/consultas",  consult.postConsulta);
+
+route.put("/consulta/:id",  consult.updateConsulta);
+
+route.delete("/consulta/:id",  consult.deleteConsulta);
+
+route.get("/", async (req, res) => {
+    return res.status(200).send({ message: "Crud is running" })
+})
